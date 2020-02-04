434f3c5762f66bab056cf34c7568e06ba7792506
KsRyY <andy200511@126.com>
2020-02-04 12:05:50 +0800

    CSS Modules 示例

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import styles from './App.module.css'
+
 function App() {
-  return (
-    <div className="app-entry">
-      <h1>Poi + React</h1>
-    </div>
-  )
+    return (
+        <div className="app">
+            <h1>Poi + React</h1>
+            <h1 className={styles.title}>Blue Title with Pale Violet Red Background</h1>
+        </div>
+    )
 }
 
 export default App
